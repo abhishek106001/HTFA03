e2777f3d6a326328e1250df178a98166e220d23e
Diwakar R <153829103+Diwakar1819@users.noreply.github.com>
2024-02-21 08:00:21 +0530

    Initial Web

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("redirectButton").addEventListener("click", function() {
+    // Change the URL to the desired redirection destination
+    window.location.href = "https://ecommer-treands-1.vercel.app/";
+});
